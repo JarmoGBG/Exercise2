c762f51ff693523827ead62ccee95cf109cfd4fa
JKL <jarmogbg1975@outlook.com>
2024-03-02 17:30:48 +0100

    Renamed solution. Created a skeleton for the main menu.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,66 @@
-﻿namespace Exercise2
+﻿namespace FlowControl
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool exitLoop = false;
+
+            
+        }
+
+        private void MainMenu()
+        {
+
+        }
+
+        private void ShowMainMenuText()
+        {
+
+        }
+
+        private void MenuLoop()
+        {
+            bool continueLoop = true;
+
+            do
+            {
+                int input = ReadUserInput();
+                
+                //Act on user input
+                switch (input)
+                {
+                    //Exit program
+                    case 0:
+                        continueLoop = false;
+                        break;
+                    //Handle all the valid input cases
+                    default:
+                        //Not a valid menu item
+                        Console.WriteLine("Bad input, not a valid menu item");
+                        break;
+                }
+
+            } while (continueLoop);
+        }
+
+        /// <summary>
+        /// Reads user input
+        /// </summary>
+        /// <returns>
+        /// -1 if input was not a number or was negative number, otherwise the positive number from input 
+        /// </returns>
+        private int ReadUserInput()
+        {
+            var input = Console.ReadLine();
+
+            //Handle input
+            //Try to parse input to int 
+            //If input is not an int, i.e is null, parsing fails or is not a positive integer, return -1
+            //Else return the parsed int
+
+            //Temp
+            return -1;
         }
     }
 }
